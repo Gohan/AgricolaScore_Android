744d86c525f5752134224b3212bb1a847f8916e9
gohan <cppgohan@gmail.com>
2013-07-12 03:12:11 +0800

    可以保存玩家选取的人数了

--- AgricolaScore/build/source/r/debug/apps/baozishan/agricola_score/R.java
+++ AgricolaScore/build/source/r/debug/apps/baozishan/agricola_score/R.java
@@ -19,7 +19,7 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f08000c;
-        public static final int home_radioButton=0x7f080003;
+        public static final int home_radioButton1=0x7f080003;
         public static final int home_radioButton2=0x7f080004;
         public static final int home_radioButton3=0x7f080005;
         public static final int home_radioButton4=0x7f080006;
@@ -43,8 +43,9 @@ public final class R {
         public static final int action_settings=0x7f050000;
         public static final int app_name=0x7f050001;
         public static final int hello_world=0x7f050002;
-        public static final int home_tv_playernumber=0x7f050003;
-        public static final int home_tv_usage=0x7f050004;
+        public static final int home_static_jsonstring=0x7f050003;
+        public static final int home_tv_playernumber=0x7f050004;
+        public static final int home_tv_usage=0x7f050005;
     }
     public static final class style {
         /** 
--- AgricolaScore/src/main/java/apps/baozishan/agricola_score/HomeActivity.java
+++ AgricolaScore/src/main/java/apps/baozishan/agricola_score/HomeActivity.java
@@ -17,25 +17,163 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
 
 import apps.baozishan.agricola_score.Utils.PlayerItem;
 
 public class HomeActivity extends Activity {
 
+    private JSONObject gameInfo = new JSONObject();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        LoadPlayerInfoOnStart(savedInstanceState);
+
         RadioGroup rg = (RadioGroup)findViewById(R.id.home_rg_playernumber);
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                String strPlayerNumber = ((RadioButton) findViewById(i)).getText().toString();
-                ShowMessageBox(strPlayerNumber);
-                SetListViewByPlayerNumber(Integer.parseInt(strPlayerNumber));
+                View item = findViewById(i);
+                if(item != null) {
+                    item.performClick();
+                    String strPlayerNumber = ((RadioButton) findViewById(i)).getText().toString();
+                    ShowMessageBox(strPlayerNumber);
+                    int nPlayerNumber = Integer.parseInt(strPlayerNumber);
+                    SetListViewByPlayerNumber(Integer.parseInt(strPlayerNumber));
+                    try {
+                        gameInfo.put("PlayerNumber", nPlayerNumber);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    SavePlayerInfo();
+                }
             }
         });
+        Dictionary<Integer, Integer> mapRadio = new Hashtable<Integer, Integer>();
+        mapRadio.put(1, R.id.home_radioButton1);
+        mapRadio.put(2, R.id.home_radioButton2);
+        mapRadio.put(3, R.id.home_radioButton3);
+        mapRadio.put(4, R.id.home_radioButton4);
+        mapRadio.put(5, R.id.home_radioButton5);
+        int number = 0;
+        try {
+            number = gameInfo.getInt("PlayerNumber");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RadioButton rb = (RadioButton) findViewById(mapRadio.get(number));
+        if (rb != null) {
+            rb.setChecked(true);
+        }
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString("LastGameInfo", gameInfo.toString());
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        LoadPlayerInfoOnStart(savedInstanceState);
+    }
+
+    private void SavePlayerInfo() {
+        PrintStream outputStream = null;
+        try {
+            outputStream = new PrintStream(openFileOutput("Save.json", MODE_PRIVATE));
+            outputStream.print(gameInfo.toString());
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void LoadPlayerInfoOnStart(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            String strGameInfoJson = savedInstanceState.getString("LastGameInfo");
+            try {
+                gameInfo = new JSONObject(strGameInfoJson);
+            } catch (JSONException e) {
+                InitPlayerInfo();
+                return;
+            }
+        }
+
+        FileInputStream stream = null;
+        try {
+            stream = openFileInput("Save.json");
+        } catch (FileNotFoundException e) {
+            InitPlayerInfo();
+            return;
+        }
+        InputStreamReader inputStreamReader = new InputStreamReader(stream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            gameInfo = new JSONObject(sb.toString());
+        } catch (JSONException e) {
+            InitPlayerInfo();
+        }
+
+
+    }
+    // String s = String.format("{\n        \"name\":\"Player%%%%d\",\n        \"color\":\"%%%%s\",\n        \"room_type\":\"Wood\",\n        \"fields\":[\n        0,\n        -1\n        ],\n        \"pastures\":[\n        0,\n        -1\n        ],\n        \"grain\":[\n        0,\n        -1\n        ],\n        \"vegetables\":[\n        0,\n        -1\n        ],\n        \"sheep\":[\n        0,\n        -1\n        ],\n        \"boar\":[\n        0,\n        -1\n        ],\n        \"cattle\":[\n        0,\n        -1\n        ],\n        \"unused_space\":[\n        0,\n        0\n        ],\n        \"fenced_stables\":[\n        0,\n        0\n        ],\n        \"rooms\":[\n        5,\n        0\n        ],\n        \"family_members\":[\n        2,\n        6\n        ],\n        \"victory_points\":0,\n        \"bonus_points\":0,\n        \"total_score\":-1\n        }")
+
+    private void InitPlayerInfo() {
+        String color[] = new String[] {"red", "blue", "yellow", "purple", "white"};
+        gameInfo = new JSONObject();
+        JSONArray players = new JSONArray();
+        for (int i = 0; i < 5; i++) {
+            players.put(CreatePlayerJsonObject(i, color[i]));
+        }
+        try {
+            gameInfo.put("PlayerNumber", players.length());
+            gameInfo.put("Player", players);
+        } catch (JSONException e1) {
+            e1.printStackTrace();
+        }
+    }
+    private JSONObject CreatePlayerJsonObject(int i, String s) {
+        String strInitInfo = String.format(getString(R.string.home_static_jsonstring), i, s);
+        JSONObject ret = null;
+        try {
+            ret = new JSONObject(strInitInfo);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return new JSONObject();
+        }
+        return ret;
     }
 
 
